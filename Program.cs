651c0ba80cd9d55c25c85dfebaeff10a7f8fe670
yemrekaradag <yemrekaradag@hotmail.com>
2025-09-27 15:30:36 +0300

    Tasks

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        Task1();
+        Task2();
+        Task3();
+        Task4();
+    }
+
+    static void Task1()
+    {
         Console.Write("Enter Positive Number: ");
 
         string? input = Console.ReadLine();
@@ -36,7 +44,100 @@ class Program
 
         PrintEvenNumbers(n, numbers);
     }
+    static void Task2()
+    {
+        Console.Write("Enter Positive Number: ");
 
+        string? input = Console.ReadLine();
+
+        if (!input.IsPositiveNumber())
+        {
+            Console.WriteLine("\nPlease enter a positive number.");
+            return;
+        }
+
+        Console.Write("Enter Positive Number2: ");
+
+        string? input2 = Console.ReadLine();
+
+        if (!input2.IsPositiveNumber())
+        {
+            Console.WriteLine("\nPlease enter a positive number.");
+            return;
+        }
+
+        int n = int.Parse(input!);
+        int m = int.Parse(input2!);
+        int[] numbers = new int[n];
+
+        Console.WriteLine($"\nPlease enter {n} numbers:");
+
+        for (int i = 0; i < n; i++)
+        {
+            string? userInput = Console.ReadLine();
+
+            if (!userInput.IsPositiveNumber())
+            {
+                Console.WriteLine("Please enter a positive number.");
+                i--;
+                continue;
+            }
+
+            numbers[i] = int.Parse(userInput!);
+        }
+
+        PrintDivisorsOrEqual(m, numbers);
+    }
+    static void Task3()
+    {
+        Console.Write("Enter Positive Number: ");
+
+        string? input = Console.ReadLine();
+
+        if (!input.IsPositiveNumber())
+        {
+            Console.WriteLine("\nPlease enter a positive number.");
+            return;
+        }
+
+        int n = int.Parse(input!);
+        string[] words = new string[n];
+
+        Console.WriteLine($"\nPlease enter {n} words:");
+
+        for (int i = 0; i < n; i++)
+        {
+            string? userInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Please enter a word.");
+                i--;
+                continue;
+            }
+
+            words[i] = userInput;
+        }
+
+        PrintWords(words);
+    }
+    static void Task4()
+    {
+        Console.Write("Enter a sentence: ");
+        string? input = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("\nPlease enter a sentence.");
+            return;
+        }
+
+        string[] arr = input.Split(' ');
+        Console.WriteLine($"Total word count: {arr.Length}");
+
+        int letterCount = input.Replace(" ", "").Length;
+        Console.WriteLine($"Total letter count: {letterCount}");
+    }
     static void PrintEvenNumbers(int n, int[] numbers)
     {
         Console.WriteLine("\nEven numbers:");
@@ -44,6 +145,26 @@ class Program
         {
             if (numbers[i] % 2 == 0)
                 Console.WriteLine(numbers[i]);
+        }
+    }
+    static void PrintDivisorsOrEqual(int m, int[] numbers)
+    {
+        Console.WriteLine($"Numbers equal to or divisible by {m}:");
+        foreach (int number in numbers)
+        {
+            if (number == m || m % number == 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+    static void PrintWords(string[] words)
+    {
+        Array.Reverse(words);
+
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
         }
     }
 }
--- obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c1072557558b20f9776ddc6b37601b81338ace")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
